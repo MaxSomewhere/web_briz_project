2de73da99052bfe71644369d4df4cdaf32f7cc1b
MaxSomewhere <maxpower199510@gmail.com>
2021-03-01 12:35:19 +0200

    1

--- cypress/integration/WebBrizTv/5.profile.js
+++ cypress/integration/WebBrizTv/5.profile.js
@@ -1,14 +1,14 @@
 const { get } = require("http")
 //comment
-describe ("{Profile", () => {
+describe ("Profile", () => {
     beforeEach(() =>  {
         cy.viewport(1920, 1080)
-        cy.visit(data.baseUrl)
+        
     })
 
     it ("Профиль",() => {
         cy.fixture('webBrizTv').then(data => {
-            
+            cy.visit(data.baseUrl)
             cy.log('Авторизация с корректными данными')
             cy.get('a[href="/auth/login"]').click()
             cy.get('input[name="login"]').clear().type(data.valid_auth_login)
@@ -18,7 +18,7 @@ describe ("{Profile", () => {
                 .should('exist')
 
             
-            
+               // cy.get('li[class="ToolBar__login"]').trigger('mouseover').get('div[class="ProfileDropdown"]').find('ul').find('li').eq(2).should('be.visible').click({force:true})
             cy.visit('https://ottdevfront.briz.ua/profile')
             
             cy.log('Проверка наличия элементов на странице')
@@ -41,7 +41,7 @@ describe ("{Profile", () => {
             cy.get('input[name="name"]').clear().type('Max')
             cy.get('input[name="email"]').clear().type(data.existing_reg_login)
             cy.get('button[type="submit"]').click()
-            cy.get('span[class="i-m-error"]').should('exist')
+            cy.get('div[class="Toastify__toast Toastify__toast--error"]').should('exist')
 
             cy.log('Поля выбора даты')
             cy.get('select[name="day"]').should('exist')
