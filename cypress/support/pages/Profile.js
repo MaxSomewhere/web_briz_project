e4aa9b5c1e799e49f448d7dbd02f394d0a0ed075
MaxSomewhere <maxpower199510@gmail.com>
2021-03-15 12:32:13 +0200

    1

--- cypress/support/pages/Profile.js
+++
@@ -1,8 +0,0 @@
-export class profileactions {
-    EditEmail(email) {
-        cy.get('input[name="name"]').clear().type('Max')
-        cy.get('input[name="email"]').clear().type(email)
-        cy.get('button[type="submit"]').click()
-    }
-}
-export const profile = new profileactions()
