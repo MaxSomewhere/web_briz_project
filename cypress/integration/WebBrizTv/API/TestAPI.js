e974a47ac1901cf9898d4af75f41c929a19fac53
MaxSomewhere <maxpower199510@gmail.com>
2021-04-12 15:59:07 +0300

    12.04

--- cypress/integration/WebBrizTv/API/TestAPI.js
+++ cypress/integration/WebBrizTv/API/TestAPI.js
@@ -1,20 +1,16 @@
-it ('Example GET request', ()=>{
-        cy.request('https://ottdevfront.briz.ua/')
+describe('HTTP Example', function(){
 
-            .then((response)=>{
-                console.log(response);
 
+    it ('Example GET request', function(){
+
+        cy.request({
+            method : 'GET',
+            url : 'https://dev-payments.briz.ua/v2/get', // baseUrl is prepended to url
+                       
+        }).then(function(response){
+
+            expect(response.body).to.have.property
         })
-})
+    });
 
-it('Example POST request', ()=>{
-    cy.request({
-        method:'POST',
-        url: 'https://ottdevfront.briz.ua/',
-        body: '',
-        headers: '',
-    })
-        .then((response)=>{
-            console.log(response)
-        })
-})
+});
