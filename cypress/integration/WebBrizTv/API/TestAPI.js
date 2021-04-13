fdfab535a5c730584c63b3d0755118b0360b046e
MaxSomewhere <maxpower199510@gmail.com>
2021-04-13 17:47:58 +0300

    13.04

--- cypress/integration/WebBrizTv/API/TestAPI.js
+++ cypress/integration/WebBrizTv/API/TestAPI.js
@@ -3,7 +3,7 @@ describe('HTTP Example', ()=> {
 
     it ('Example GET request', ()=> {
 
-        cy.request('http://dbilling.briz.ua/Ru/#/', { failOnStatusCode: false })
+        cy.request('https://dev-payments.briz.ua/v2/monobank_payment.php?command=pay&txn_id=4294937295&txn_date=20201010140023&account=1234&sum=133')
             .then((response)=>{
 
             })
