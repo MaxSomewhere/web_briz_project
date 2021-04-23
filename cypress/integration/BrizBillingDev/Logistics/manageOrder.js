3bbfcd11e657694299ecf2468284a4dffe21e336
MaxSomewhere <maxpower199510@gmail.com>
2021-04-23 09:29:06 +0300

    23.04

--- cypress/integration/BrizBillingDev/Logistics/manageOrder.js
+++ cypress/integration/BrizBillingDev/Logistics/manageOrder.js
@@ -10,19 +10,24 @@ describe ("Order manage", () => {
         cy.fixture('brizbilling').then(bill => {
             cy.visit("http://dbilling.briz.ua/Ru/#/", { failOnStatusCode: false })
                 DevBillLog.devLogin(bill.logisticsManager, bill.dbillingPass)
+            })
                 cy.get('[data-cy=side-menu-logistics-index]').click()
                 cy.get('[data-cy=side-menu-logistics-BillVatApprove]').click()
                 cy.get('.el-input-group__append > .el-button').click()
                 cy.get(':nth-child(2) > .el-col-xs-6 > .hidden-xs-only').click()
                 cy.get('.el-col-md-6 > .el-input > .el-input__inner').clear().type('3400')
 
-                cy.fixture('image9mb.jpg').then(fileContent => {
-                    cy.get('input[class="el-input__inner"]').attachFile('image9mb.jpg')
+                cy.fixture('testimage9.jpeg').then((fileContent) => {
+                    cy.get('.el-upload > .el-input > .el-input__inner')
+                    .attachFile(
+                        { fileContent, mimeType: 'image/jpeg' },
+                        { subjectType: 'input', force: true }
+                      )
+                
                 })
                 
                 
-                
-        })
+        
     })
 
 })
